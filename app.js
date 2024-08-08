bd8d2d837e8314762f4bf25bc1696ebf8cce3fd0
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-08-08 23:05:22 +0400

    updated checking email code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("myinput");
 const error = document.getElementById("error-message");
+const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 input.addEventListener("change", () => {
     error.textContent = "";
@@ -8,9 +9,9 @@ input.addEventListener("change", () => {
 function check(){
     if(input.value.trim() == ""){
         error.textContent = "Please provide your email";
-    }else if(!input.value.trim().endsWith('@gmail.com')){
-        error.textContent = "Oops! Please check your email";
     }else if(input.value.trim().length < 15){
         error.textContent = "Oops! Email is too short. Try again!";
+    }else if(!regex.test(input.value.trim())){
+        error.textContent = "Oops! Please check your email";
     }
 }
